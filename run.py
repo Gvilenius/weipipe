64b9017812ad7c25e66d9954f0f27abd34fe7590
linjf <linjf21@mails.tsinghua.edu.cn>
2024-02-21 00:43:11 +0800

    feat: backward half finished

--- run.py
+++ run.py
@@ -6,39 +6,59 @@ import torch.distributed.rpc as rpc
 import tempfile
 import torch.distributed as dist
 import argparse
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.distributed import DistributedSampler
 
-# tmpfile = tempfile.NamedTemporaryFile()
-# rpc.init_rpc(
-#     name="worker",
-#     rank=0,
-#     world_size=1,
-#     rpc_backend_options=rpc.TensorPipeRpcBackendOptions(
-#         init_method="file://{}".format(tmpfile.name),
-#         _transports=["ibv", "uv"],
-#         _channels=["cuda_ipc", "cuda_basic"],
-#     )
-# )
+
+torch.manual_seed(123)
+
+
+class MyData(Dataset):
+    def __init__(self):
+        n_samples = 10
+        seq_len = 3
+        input = torch.randint(0, 10, (n_samples, seq_len + 1))
+        enc_input = input[:, :-1].long()
+        targets = input[:, 1:].long()
+        self.data = list(zip(enc_input, targets))
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return len(self.data)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument ("--local_rank", default=-1, type=int)
-parser.add_argument ("--rank", default=-1, type=int)
+parser.add_argument("--local_rank", default=-1, type=int)
+parser.add_argument("--rank", default=-1, type=int)
 args = parser.parse_args()
-dist.init_process_group (
-    backend="nccl"
+
+
+tmpfile = tempfile.NamedTemporaryFile()
+rpc.init_rpc(
+    name="worker",
+    rank=0,
+    world_size=1,
+    rpc_backend_options=rpc.TensorPipeRpcBackendOptions(
+        init_method="file://{}".format(tmpfile.name),
+        _transports=["ibv", "uv"],
+        _channels=["cuda_ipc", "cuda_basic"],
+    ),
 )
 
-torch.cuda.set_device (dist.get_rank())
+
+def init_process_group():
+    dist.init_process_group(backend="nccl")
+    torch.cuda.set_device(dist.get_rank())
 
 
-
+init_process_group()
 
 model = WeiPipe()
-
-with torch.no_grad():
-    model.forward(torch.tensor([1.0, 2.0, 3.0]).cuda())
-
-# x = torch.Tensor([1.0]*10).cuda()
-# y_ref = model(x)
-
-# print(x, y_ref.local_value())
+ds = MyData()
+sampler = DistributedSampler(ds)
+dataloader = DataLoader(ds, batch_size=2, sampler=sampler)
+for x, y in dataloader:
+    model.forward_backward_step(x.cuda().half(), y.cuda().half())
+    break
--- weipipe/weipipe.py
+++ weipipe/weipipe.py
@@ -1,31 +1,49 @@
-import os
 import torch
 import torch.nn as nn
 import torch.distributed as dist
+import torch.nn.functional as F
 from pprint import pprint
 
 
-def module_to_tensor(module):
-    return torch.hstack ([p.data.flatten() for p in module.parameters()])
+def models_to_tensor(models):
+    return torch.hstack([model_to_tensor(model) for model in models])
 
-def tensor_to_module (tensor, module):
+
+def model_to_tensor(model):
+    return torch.hstack(
+        [p.data.flatten().to(torch.float16) for p in model.parameters()]
+    )
+
+
+def tensor_to_model(tensor, model):
     i = 0
-    for p in module.parameters():
-        dim = p.data.dim()
+    for p in model.parameters():
         n = p.data.numel()
-        raw = tensor.narrow (0, i, n)
+        raw = tensor.narrow(0, i, n)
         i += n
-
-        # [1.0] -> [[1.0]]
-        for _ in range (dim-1):
-            raw = raw.unsqueeze(0)
         p.data = raw.reshape(p.data.shape)
 
-def print_parameters(module):
-    print ("rank:", dist.get_rank())
-    for p in module.parameters():
-        pprint(p)
-    print()
+
+def tensor_to_models(tensor, models):
+    i = 0
+    for model in models:
+        for p in model.parameters():
+            n = p.data.numel()
+            raw = tensor.narrow(0, i, n)
+            i += n
+            p.data = raw.reshape(p.data.shape)
+
+
+class Layer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(3, 3, bias=False)
+        data = dist.get_rank() * torch.ones(self.linear.weight.shape)
+        self.linear.weight.data = data
+
+    def forward(self, x):
+        return self.linear(x)
+
 
 class WeiPipe(nn.Module):
     def __init__(self) -> None:
@@ -34,85 +52,128 @@ class WeiPipe(nn.Module):
         self.world_size = dist.get_world_size()
         self.rank = dist.get_rank()
 
-        self.model = nn.Sequential (
-            nn.Linear(3,128),
-            nn.Linear(128,128),
-            nn.Linear(128,3)
-        ).cuda()
-        
+        self.model_fp32 = Layer().cuda().to(dtype=torch.float32)
+        # self.model = self.model_fp32.to(dtype=torch.float16)
+        self.models = [
+            self.model_fp32.to(torch.float16),
+            Layer().cuda().to(dtype=torch.float16),
+        ]
+        self.current_model_index = 0
+
         self.activations = []
-        self.flattened_model = [module_to_tensor (self.model), None]
-        self.flattened_model[1] = torch.empty (len(self.flattened_model[0])).cuda()
-        self.state = - self.rank
-        self.loss_fn = lambda x : x
+        self.gradients = []
+        self.flattened_model = [None, None]
 
-    def forward (self, x):
-        self.activations.append (x)
-        for _ in range (self.world_size * 2 - 1):
-            with torch.no_grad():
-                self.forward_step (self.activations[-1])
-        
-        loss  = self.loss_fn(self.activations.pop())
+        self.state = -1 * self.rank
+        self.loss_fn = F.cross_entropy
+        self.optimizer = torch.optim.SGD(
+            self.model_fp32.parameters(), lr=0.1, momentum=0.9
+        )
+        self.optimizer.zero_grad()
 
-        self.backward(loss)
-        print(self.activations)
-        
+    def weight_swap(self):
+        """At the begining, swap weight between rank i and rank n-i"""
+        dst_rank = self.world_size - 1 - self.rank
+
+        self.flattened_model[0] = model_to_tensor(self.models[0])
+        self.flattened_model[1] = (
+            torch.empty(len(self.flattened_model[0])).half().cuda()
+        )
+
+        send_op = dist.P2POp(dist.isend, self.flattened_model[0], dst_rank)
+        recv_op = dist.P2POp(dist.irecv, self.flattened_model[1], dst_rank)
+        reqs = dist.batch_isend_irecv([send_op, recv_op])
+        for r in reqs:
+            r.wait()
+        tensor_to_model(self.flattened_model[1], self.models[1])
+        self.print_model(True)
+
+    def weight_flow(self):
+        prev_rank = (self.rank + self.world_size - 1) % self.world_size
+        next_rank = (self.rank + 1) % self.world_size
+        self.flattened_model[0] = models_to_tensor(self.models)
+        self.flattened_model[1] = (
+            torch.empty(len(self.flattened_model[0])).half().cuda()
+        )
+        send_op = dist.P2POp(dist.isend, self.flattened_model[0], next_rank)
+        recv_op = dist.P2POp(dist.irecv, self.flattened_model[1], prev_rank)
+        reqs = dist.batch_isend_irecv([send_op, recv_op])
+        for r in reqs:
+            r.wait()
+        tensor_to_models(self.flattened_model[1], self.models)
+
+    def is_pipeline_last_stage(self):
+        return self.rank == self.world_size - 1
+
+    def is_pipeline_first_stage(self):
+        return self.rank == 0
+
+    def forward(self, x):
+        return self.models[self.current_model_index](x)
+
+    def forward_model(self):
+        self.current_model_index = 0
+
+    def backward_model(self):
+        self.current_model_index = 1
+
+    def print_model(self, all=False):
+        model_str = str(*self.models[self.current_model_index].parameters())
+        if self.current_model_index == 0:
+            msg = f"rank{self.rank} forward using {model_str}"
+        else:
+            msg = f"rank{self.rank} backward using {model_str}"
+
+        if all:
+            model_str0 = str(*self.models[0].parameters())
+            model_str1 = str(*self.models[1].parameters())
+            msg = f"rank{self.rank} all model parameter is {model_str0} {model_str1}"
+
+        print(msg)
+
+    def forward_backward_step(self, inputs, targets=None):
+        self.weight_swap()
+        self.activations.append(inputs)
+        for i in range(self.world_size * 3):
+            i_offset = i - self.rank
+
+            # calculate loss
+            if i_offset == self.world_size:
+                outputs = self.activations.pop()
+                outputs.requires_grad = True
+                self.loss_fn(outputs, targets).backward()
+                grad = outputs.grad
+
+            # forward
+            if 0 <= i_offset < self.world_size:
+                self.forward_model()
+                with torch.no_grad():
+                    x = self.activations[-1]
+                    y = self.forward(x)
+                self.activations.append(y)
+            # backward
+            elif self.world_size <= i_offset < self.world_size * 2:
+                self.backward_model()
+                inputs = self.activations.pop().detach()
+                inputs.requires_grad = True
+
+                outputs = self.forward(inputs)
+                outputs.backward(grad)
+                grad = inputs.grad
+            self.weight_flow()
+        # backward *world_size* times
+        # recomputation
+
+        return outputs
+
     def backward(self, x):
         x = self.activations.pop().detach()
-        self.forward_step (x)
+        self.forward_step(x)
         y = self.activations.pop()
         loss = self.loss_fn(y)
         loss.backward()
-    
-
-    def forward_step(self, x):
-        if self.state >= 0 and self.state < self.world_size:
-            y = self.model(x)
-            self.activations.append(y)
-            
-        self.state += 1
-        
-        self.flattened_model[0] = module_to_tensor(self.model)
-        send_op = dist.P2POp(
-            dist.isend, self.flattened_model[0], (self.rank + 1) % self.world_size
-        )
-        recv_op = dist.P2POp(
-            dist.irecv,
-            self.flattened_model[1],
-            (self.rank + self.world_size - 1) % self.world_size,
-        )
-        reqs = dist.batch_isend_irecv([send_op, recv_op])
-        for req in reqs:
-            req.wait()
-        tensor_to_module (self.flattened_model[1], self.model)
-
-    def backward_step(self, dx):
-        pass
-
-from torch.utils.data import Dataset, DataLoader
-class MyData(Dataset):
-    def __init__(self):
-        n_samples = 5
-        seq_len = 10
-        input = torch.randint(0, 10, (n_samples, seq_len + 1))
-        enc_input = input[:, :-1].long()
-        targets = input[:, 1:].long()
-        self.data = list(zip (enc_input, targets))
-    def __getitem__(self, index):
-        return self.data[index]
-    
-    def __len__(self):
-        return len(self.data)
-
-    
-class MyDataLoader(DataLoader):
-    def __init__(self):
-        pass
+        self.optimizer.step()
 
 
 if __name__ == "__main__":
-    torch.manual_seed (1234)
-    d = MyData()
-    dl = DataLoader (d, batch_size=2)
-    for x in dl:
-        print(x)
+    pass
